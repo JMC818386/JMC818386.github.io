a7594314bc9b2da20892aaf35620b8f7363fef9f
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-04-29 13:36:36 +0000

    Test

--- my-app/src/Components/Contact.js
+++ my-app/src/Components/Contact.js
@@ -9,7 +9,7 @@ function Contact() {
       <div className="container-fluid p-5 d-flex align-items-center justify-content-center bg-dark text-light" style={{ height: "90vh" }}>
         <div className="row">
           <div className="col-12">
-            <h1 className="d-flex justify-content-center" style={{ fontSize: 80 }}>Contact</h1>
+            <h1 className="d-flex justify-content-center" style={{ fontSize: 80 }}>Contact Info</h1>
           </div>
         </div>
       </div>
