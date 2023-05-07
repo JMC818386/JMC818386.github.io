935a69837ed1786c2d89873df0a795154094a58e
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-05-07 16:59:33 +0000

    Adjusted main title text     Adjusted line-height and responsive sizing

--- my-app/src/Components/Home.js
+++ my-app/src/Components/Home.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import TopNav from './TopNav';
 import Footer from './Footer';
 import './App.css';
+import HeroName from './img/HeroName.png';
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
       <div className="container-fluid p-5 d-flex align-items-center justify-content-center bg-dark text-light w-100" style={{ height: "80vh" }}>
         <div className="row">
           <div className="col-12">
+            {/* <img className="mx-5 my-4"src={HeroName} style={{ width: 1200 }} alt="Logo" /> */}
             <p className="d-flex justify-content-center text-header mb-0 pb-0">John-Michael Crawford</p>
             <p className="d-flex justify-content-center text-secondary mt-0 pt-0">Graphic Designer & Web Developer</p>
           </div>
