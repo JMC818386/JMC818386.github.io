f89dcd1a3ad1e9b7a16613436bad2124ff3577e1
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-05-10 12:07:39 +0000

    Fixed Home title text and bg coverage

--- my-app/src/Components/Home.js
+++ my-app/src/Components/Home.js
@@ -7,13 +7,13 @@ import ThreeDname from './img/3Dname.png';
 
 function Home() {
   return (
-    <div>
+    <div className="bg-dark">
       <TopNav />
-      <div className="container p-5 d-flex align-items-center justify-content-center bg-dark text-light vh-100">
+      <div className="container p-5 d-flex align-items-center justify-content-center bg-dark text-light">
         <div className="row d-flex justify-content-center">
-          <div className="col-12 d-flex flex-wrap justify-content-center hero-name">
-            <img className="mx-5 mb-0" src={ThreeDname} style={{ maxWidth: "100%" }} alt="Logo" />
-            {/* <p className="d-flex justify-content-center text-header mb-0 pb-0">John-Michael Crawford</p> */}
+          <div className="col-12 d-flex flex-wrap justify-content-center hero-name mt-5 pt-5">
+            {/* <img className="mx-5 mb-0" src={ThreeDname} style={{ maxWidth: "100%" }} alt="Logo" /> */}
+            <p className="d-flex justify-content-center text-header mb-0 pb-0 mt-5 pt-5">John-Michael Crawford</p>
             <p className="d-flex justify-content-center text-secondary mt-0 pt-0">Graphic Designer & Web Developer</p>
           </div>
         </div>
