ba6d24312e96c429eb4427be5c275ad5ad4633d4
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2025-07-02 04:29:47 +0000

    Switched from lightroom to full pages

--- my-app/src/Components/Portfolio.js
+++ my-app/src/Components/Portfolio.js
@@ -2,16 +2,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import TopNav from './TopNav';
 // import PortfolioNav from './PortfolioNav';
 import Footer from './Footer';
-import WebDevelopmentBody from './WebDevelopmentBody';
 import './App.css';
 import Grid from './img/Grid.png';
+import GraphicDesignBody from './GraphicDesignBody';
+import WebDevelopmentBody from './WebDevelopmentBody';
 
 function Portfolio() {
   return (
   <div style={{backgroundImage: `url(${Grid})`, backgroundSize: 'cover'}}>
     <TopNav />
     {/* <PortfolioNav /> */}
-    <WebDevelopmentBody />
+    {/* <WebDevelopmentBody /> */}
+    <GraphicDesignBody />
     <Footer />
   </div>
   );
